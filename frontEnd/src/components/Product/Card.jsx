5cb204748e8a44e6bde6c53a524dc9b31b6f6c53
yassineyahyaoui <yassineyahyaoui40@gmail.com>
2025-05-06 16:15:27 +0100

    img testing

--- frontEnd/src/components/Product/Card.jsx
+++ frontEnd/src/components/Product/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card(props) {
   return (
     <div className="card bg-light m-1 pb-2" style={{width: '14rem'}}>
-        <div><img className='card-img-top' src="#" alt='card image cap'></img></div>
+        <div><img className='card-img-top' src={props.img} alt='card image cap'></img></div>
         <div className='card-body' ><h1 className='card-title'>{props.title}</h1>
         <p className='card-text'>{props.text}</p>
         <p className='card-text'>{props.rate}</p></div>
